325ac8db5e6fc7c7ae93e40092a2bea5a090c7fa
aligurbuz <galiant781@gmail.com>
2016-12-07 11:58:35 +0000

    composer.json has been updated

--- src/app/v1/bar/index.php
+++ src/app/v1/bar/index.php
@@ -3,9 +3,15 @@ namespace src\app\v1\bar;
 
 class index extends app {
 
+    private $container;
+
+    public function __construct(){
+
+        $this->container=\DI\ContainerBuilder::buildDevContainer();
+    }
+
     public function get(){
 
-        $container = \DI\ContainerBuilder::buildDevContainer();
-        return \src\config\config::get("app");
+        return $this->container->get("\\src\\app\\v1\\bar\\test")->inst();
     }
 }
---
+++ src/app/v1/bar/test.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace src\app\v1\bar;
+
+class test {
+    public function inst(){
+        return 'hello world';
+    }
+}
