a23c6f244f425e90836f625258f3da5100145656
aligurbuz <galiant781@gmail.com>
2016-12-08 12:12:37 +0000

    lib/bin

---
+++ src/commands/page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Command write.
+ * type array
+ * package:command runner
+ * user apix
+ */
+
+class page {
+
+    //page create command
+    public function create ($data){
+
+        //return
+        return 'command test';
+    }
+}
