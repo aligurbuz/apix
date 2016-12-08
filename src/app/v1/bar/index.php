6caa64c06f0765873ca3bff79ae5eb27bbba57c5
aligurbuz <galiant781@gmail.com>
2016-12-08 12:36:49 +0000

    directory structe has been edited

--- src/app/v1/bar/index.php
+++ src/app/v1/bar/index.php
@@ -1,19 +1,17 @@
 <?php
 namespace src\app\v1\bar;
-use src\app\v1\bar\test as test;
 
 class index extends app {
 
     public $test;
 
-    public function __construct(test $test){
+    public function __construct(){
 
-        $this->test=$test;
     }
 
     public function get(){
 
         //di container
-        return $this->test->inst();
+        return 'hello world';
     }
 }
---
+++ src/config/database.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: erol
+ * Date: 08/12/16
+ * Time: 14:36
+ */
