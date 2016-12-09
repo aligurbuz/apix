e932c3dbf226f29a2b523433533d53be7b3435da
aligurbuz <galiant781@gmail.com>
2016-12-09 10:34:46 +0000

    structured

--- lib/connection.php
+++ lib/connection.php
@@ -4,14 +4,31 @@ namespace lib;
 
 class connection {
 
+    /**
+     * connect to api service (created service).
+     *
+     * Enabled by default.
+     *
+     * @param bool $bool
+     * @return connectin runner
+     */
     public static function run() {
 
+        //service main file extends this file
         require(root . '/src/app/trendmax/v1/bar/app.php');
+
+        //service main file
         require(root . '/src/app/trendmax/v1/bar/index.php');
+
+        //resolve process
         $resolve=require(root.'/lib/resolver.php');
         $resolve=new \classresolver();
         $apix=$resolve->resolve("\\src\\app\\trendmax\\v1\\bar\\index");
+
+        //check config file for method name
         $mainFunctionMethod=\src\config\config::get("mainFunctionMethod");
+
+        //call service
         return $apix->$mainFunctionMethod();
     }
 }
