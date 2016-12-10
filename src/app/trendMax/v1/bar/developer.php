b69be750cb3ed53ae364e8d2bc4fa2259ed18b5c
Ali Gurbuz <galiant781@gmail.com>
2016-12-10 11:00:17 +0000

    structure has been edited

---
+++ src/app/trendMax/v1/bar/developer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * service developer.
+ * User: developer
+ */
+
+namespace src\app\trendmax\v1\bar;
+
+
+class developer
+{
+
+}
---
+++ src/app/trendMax/v1/serviceBaseController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Service base controller
+ * it is mainly service provider for service
+ * service provider
+ */
+
+namespace src\app\trendmax\v1;
+
+
+class serviceBaseController
+{
+
+}
