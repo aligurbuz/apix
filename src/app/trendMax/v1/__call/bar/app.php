761d99a872019a7b6c1382cc16e29304b30deec3
Ali Gurbuz <galiant781@gmail.com>
2016-12-11 12:22:06 +0000

    structure has been edited

--- src/app/trendMax/v1/__call/bar/app.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace src\app\trendmax\v1\__call\bar;
-
-class app {
-
-
-
-}
--- src/app/trendMax/v1/__call/bar/branches/handle/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: erol
- * Date: 08/12/16
- * Time: 14:33
- */
