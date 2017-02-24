337888c5de75a5186ead1a6bc128ca9e5081e473
aligurbuz <galiant781@gmail.com>
2017-02-24 07:55:49 +0000

    apix structured

--- src/services/redis.php
+++ src/services/redis.php
@@ -25,7 +25,7 @@ class redis {
     /**
      * redis configuration.
      *
-     * @return redis class
+     * @return redis @connection
      */
     public function __construct(client $client){
 
@@ -35,9 +35,28 @@ class redis {
 
 
     /**
-     * redis set data.
+     * Returns PONG if no argument is provided, otherwise return a copy of the argument as a bulk.
+     * This command is often used to test if a connection is still alive, or to measure latency.
+     * If the client is subscribed to a channel or a pattern, it will instead return a multi-bulk with a "pong"
+     * in the first position and an empty bulk in the second position, unless an argument is
+     * provided in which case it returns a copy of the argument.
      *
-     * @return redis class
+     * @return redis @data
+     */
+    public function ping($message='hello redis'){
+
+        //get ping return
+        return $this->client->ping($message);
+
+    }
+
+
+    /**
+     * Set key to hold the string value. If key already holds a value, it is overwritten,
+     * regardless of its type. Any previous time to live associated with the key is
+     * discarded on successful SET operation.
+     *
+     * @return redis @data
      */
     public function set($data,$expire=null){
 
@@ -54,15 +73,34 @@ class redis {
 
     }
 
+    /**
+     * If key already exists and is a string, this command appends the value at the end of the string.
+     * If key does not exist it is created and set as an empty string,
+     * so APPEND will be similar to SET in this special case.
+     *
+     * @return redis @data
+     */
+    public function append($key=null,$value=null){
+
+        //get append
+        if($key!==null && $value!==null){
+            return $this->client->append($key,$value);
+        }
+        return null;
+
+    }
+
 
     /**
-     * redis get data.
+     * Get the value of key. If the key does not exist the special value nil is returned.
+     * An error is returned if the value stored at key is not a string,
+     * because GET only handles string values.
      *
-     * @return redis class
+     * @return redis @data
      */
     public function get($key=null){
 
-        //get return
+        //get data
         if($key!==null){
             return $this->client->get($key);
         }
@@ -72,9 +110,15 @@ class redis {
     }
 
     /**
-     * redis get data exists.
+     * Returns if key exists.
+     * Since Redis 3.0.3 it is possible to specify multiple keys instead of a single one.
+     * In such a case, it returns the total number of keys existing.
+     * Note that returning 1 or 0 for a single key is just a special case of the variadic usage,
+     * so the command is completely backward compatible.
+     * The user should be aware that if the same existing key is mentioned in the arguments multiple times,
+     * it will be counted multiple times. So if somekey exists, EXISTS somekey somekey will return 2.
      *
-     * @return redis class
+     * @return redis @data
      */
     public function exists($key){
 
@@ -87,7 +131,9 @@ class redis {
 
 
     /**
-     * redis get data expire.
+     * Set a timeout on key. After the timeout has expired,
+     * the key will automatically be deleted. A key with an associated timeout is often said
+     * to be volatile in Redis terminology.
      *
      * @return redis class
      */
@@ -98,20 +144,51 @@ class redis {
     }
 
     /**
-     * delete all keys.
+     * Returns the remaining time to live of a key that has a timeout.
+     * This introspection capability allows a Redis client to check
+     * how many seconds a given key will continue to be part of the dataset.
+     *
+     * @return redis @data
+     */
+    public function ttl($key=null){
+
+        //get remaining time
+        if($key!==null){
+            return $this->client->ttl($key);
+        }
+        return null;
+
+    }
+
+    /**
+     * Delete all the keys of the currently selected DB. This command never fails.
+     * The time-complexity for this operation is O(N), N being the number of keys in the database.
      *
      * @return redis class
      */
     public function flushdb(){
 
-        //get flushdb
+        //get flush db
         $this->client->flushdb();
     }
 
     /**
-     * delete specified keys.
+     * Available since 1.0.0.
+     * Delete all the keys of all the existing databases, not just the currently selected one. This command never fails.
+     * The time-complexity for this operation is O(N), N being the number of keys in all existing databases..
      *
-     * @return redis class
+     * @return redis @data
+     */
+    public function flushAll(){
+
+        //get flush All
+        $this->client->flushall();
+    }
+
+    /**
+     * Removes the specified keys. A key is ignored if it does not exist.
+     *
+     * @return redis @data
      */
     public function delete($keys=array()){
 
@@ -122,14 +199,48 @@ class redis {
     }
 
     /**
-     * get all keys.
+     * Returns all keys matching pattern.
+     * While the time complexity for this operation is O(N), the constant times are fairly low.
+     * For example, Redis running on an entry level laptop can scan a 1 million key database in 40 milliseconds.
+     * Warning: consider KEYS as a command that should only be used in production environments with extreme care.
+     * It may ruin performance when it is executed against large databases.
+     * This command is intended for debugging and special operations, such as changing your keyspace layout.
+     * Don't use KEYS in your regular application code.
+     * If you're looking for a way to find keys in a subset of your keyspace, consider using SCAN or sets.
      *
-     * @return redis class
+     * @return redis @data
      */
     public function getAllKeys($pattern="*"){
 
         //get keys
         return $this->client->keys($pattern);
+    }
+
+    /**
+     * Serialize the value stored at key in a Redis-specific format and return it to the user.
+     * The returned value can be synthesized back into a Redis key using the RESTORE command.
+     *
+     * @return redis @data
+     */
+    public function dump($key=null){
+
+        //get dump
+        if($key!==null){
+            return $this->client->dump($key);
+        }
+        return null;
+
+    }
+
+    /**
+     * Return a random key from the currently selected database
+     *
+     * @return redis @data
+     */
+    public function getRandomKey(){
+
+        //get keys
+        return $this->client->randomkey();
     }
 
 
