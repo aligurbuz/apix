426258cee650d0d5cee2fd9d45e2a301ab895d02
aligurbuz <galiant781@gmail.com>
2017-02-24 13:15:47 +0000

    apix structured

--- src/packages/providers/search/elasticSearch/search.php
+++ src/packages/providers/search/elasticSearch/search.php
@@ -1,7 +1,43 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: erol
- * Date: 24/02/17
- * Time: 15:03
+/*
+ * This is the official PHP client for Elasticsearch. It is designed to be a very low-level client that does not stray from the REST API.
+ * All methods closely match the REST API, and furthermore, match the method structure of other language clients (ruby, python, etc).
+ * We hope that this consistency makes it easy to get started with a client, and to seamlessly switch from one language to the next with minimal effort.
+ * The client is designed to be "unopinionated". There are a few universal niceties added to the client (cluster state sniffing,
+ * round-robin requests, etc) but largely it is very barebones. This was intentional. We want a common base that more sophisticated libraries can build on top of.
  */
+
+namespace src\packages\providers\search\elasticSearch;
+
+/**
+ * Represents a elastic search class.
+ *
+ * main call
+ * return type string
+ */
+
+class search {
+
+    public $client;
+
+    /**
+     * elastic search class.
+     *
+     */
+    public function __construct(){
+
+        //search client
+        $this->client=\Elasticsearch\ClientBuilder::create()->build();
+    }
+
+    /**
+     * elastic search ping.
+     *
+     */
+    public function ping(){
+
+        //search ping
+        return 'ping';
+    }
+
+}
