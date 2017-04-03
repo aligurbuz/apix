db46948bd3230c234bc588c58d71fcc5df298c6a
aligurbuz <galiant781@gmail.com>
2017-04-03 11:27:18 +0000

    apix structured

--- lib/index.php
+++ lib/index.php
@@ -12,5 +12,17 @@
     |
     */
 
+    /*
+    |--------------------------------------------------------------------------
+    | Register The Auto Loader
+    |--------------------------------------------------------------------------
+    |
+    | Composer provides a convenient, automatically generated class loader for
+    | our application. We just need to utilize it! We'll simply require it
+    | into the script here so that we don't have to worry about manual
+    | loading any of our classes later on. It feels great to relax.
+    |
+    */
+
     require(root.'/lib/autoload.php');
     echo \lib\connection::run();
