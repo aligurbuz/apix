54c0bac0accb9aebbec7b2d7763b27d77b04e3b8
aligurbuz <galiant781@gmail.com>
2017-04-03 12:00:22 +0000

    apix structured

--- lib/utils.php
+++ lib/utils.php
@@ -4,6 +4,12 @@ namespace lib;
 
 class utils {
 
+    /**
+     * Class resolve.
+     * PHP-DI's container is preconfigured for "plug'n'play", i.e. development environment.
+     * By default, PHP-DI will have Autowiring enabled (annotations are disabled by default).
+     * return type container
+     */
     public static function resolve($class=null){
         if($class!==null){
             $container = \DI\ContainerBuilder::buildDevContainer();
