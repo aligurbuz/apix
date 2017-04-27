4529bb445e1f8881322e232089ed56e9f774fb79
aligurbuz <galiant781@gmail.com>
2017-04-27 12:05:48 +0000

    apix structured

---
+++ lib/bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Cli composer vendor autoload.
+ * For libraries that specify autoload information, Composer generates a vendor/autoload.php file.
+ * You can simply include this file and start using the classes that those libraries provide without any extra work
+ * system main skeleton
+ * return autoload file
+ */
+require_once './vendor/autoload.php';
+
+/**
+ * Spl auto load register.
+ * spl_autoload_register — Register given function as __autoload() implementation
+ * Register a function with the spl provided __autoload queue. If the queue is not yet activated it will be activated.
+ * If your code has an existing __autoload() function then this function must be explicitly registered on the __autoload queue.
+ * This is because spl_autoload_register() will effectively replace the engine cache for the __autoload() function
+ * by either spl_autoload() or spl_autoload_call()
+ * return autoload
+ */
+spl_autoload_register(function($class) {
+    $class='./'.$class.'.php';
+    $class=str_replace("\\","/",$class);
+    require($class);
+
+});
+
