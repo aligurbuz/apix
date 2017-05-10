73864fed24e3865430e142653ce9129cbfb03bef
aligurbuz <galiant781@gmail.com>
2017-05-10 14:22:06 +0000

    apix structured

--- lib/autoload.php
+++ lib/autoload.php
@@ -21,11 +21,16 @@
         $class=str_replace("\\","/",$class);
 
         if(!file_exists($class)){
+
             $alias=str_replace(root.'/','',$class);
             $alias=str_replace('.php','',$alias);
+
+            //check system app control for class alias
             $systemApp=\src\store\config\app::getClassAliasLoader();
             $appAlias='\\src\\app\\'.app.'\\'.version.'\\config\\app';
             $systemApp=array_merge($systemApp,$appAlias::getAppClassAlias());
+
+            //set class_alias groups
             if(array_key_exists($alias,$systemApp)){
                 class_alias($systemApp[$alias],$alias);
             }
