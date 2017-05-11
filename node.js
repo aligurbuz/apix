4307fd3c7946637cd15fec2c2d8d51ab754cae69
aligurbuz <galiant781@gmail.com>
2017-05-11 12:08:10 +0000

    apix structured

--- node.js
+++ node.js
@@ -1,3 +1,67 @@
+
 /**
- * Created by erol on 11/05/17.
+ * get path for main directory.
+ *
+ * @param {object} req
+ * @public
  */
+
+/**
+ * get path for main directory.
+ *
+ * @param {object} req
+ * @public
+ */
+global.path = require('path');
+global.appDir = path.dirname(require.main.filename);
+
+/**
+ * Start express project.
+ *
+ * @param {object} req
+ * @public
+ */
+var express=require("express");
+
+/**
+ * get express method--top level
+ *
+ * @param {object} req
+ * @public
+ */
+var app=express();
+
+var http = require('http').Server(app);
+
+/**
+ * express route body parser .
+ *
+ * @param {object} req
+ * @public
+ */
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+var favicon = require('serve-favicon');
+app.use(favicon(__dirname + '/favicon.ico'));
+
+/**
+ * express route .
+ *
+ * @param {object} req
+ * @public
+ */
+app.all("/",function (request,response,next){
+
+  if(request.params.name=="favicon.ico")
+  {
+    response.send("favicon.ico")
+  }
+
+  response.setHeader('Content-Type', 'application/json');
+  response.json({"success":true,"message":"hello apix"});
+
+});
+
+http.listen(3000);
