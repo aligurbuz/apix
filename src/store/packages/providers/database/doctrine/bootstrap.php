24c2ee43e67c38c1c08833cf146079c826eb287d
Vagrant User <vagrant@php7dev>
2017-06-14 14:46:27 +0000

    apix structured

--- src/store/packages/providers/database/doctrine/bootstrap.php
+++ src/store/packages/providers/database/doctrine/bootstrap.php
@@ -1,11 +1,15 @@
 <?php
 // bootstrap.php
-require_once "vendor/autoload.php";
+require_once "./vendor/autoload.php";
+
 
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
 
-$paths = array("./src/app/mobi/v1/model/doctrine");
+$paths = array("");
 $isDevMode = false;
 
 // the connection configuration
@@ -17,4 +21,9 @@ $dbParams = array(
 );
 
 $config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+$driver = new AnnotationDriver(new AnnotationReader(), $paths);
+
+// registering noop annotation autoloader - allow all annotations by default
+AnnotationRegistry::registerLoader('class_exists');
+$config->setMetadataDriverImpl($driver);
 $entityManager = EntityManager::create($dbParams, $config);
