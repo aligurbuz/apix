2eabda33345cc35dd5907c525da113cafeb0a452
aligurbuz <galiant781@gmail.com>
2017-07-18 11:10:22 +0000

    apix structured

--- src/helpers.php
+++ src/helpers.php
@@ -2,7 +2,6 @@
 if (!function_exists('dd')) {
     function dd()
     {
-        header("Content-Type: text/plain");
         $args = func_get_args();
         call_user_func_array('dump', $args);
         die();
