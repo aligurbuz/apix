b7e93a52f62d817262cd5aeae29466880d927565
aligurbuz <galiant781@gmail.com>
2017-07-28 11:11:14 +0000

    apix structured

--- src/store/config/runner.php
+++ src/store/config/runner.php
@@ -7,8 +7,6 @@
 return [
     'mixed'=>[
         'php api project create mobi',
-        'php api service create mobi:stk',
-        'php api model create mobi file:user table:users',
-        'php api migration pull:mobi --seed'
+        'php api service create mobi:stk'
     ]
 ];
