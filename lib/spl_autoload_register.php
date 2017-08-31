59cd8f7c8d3a5870cb8afd54b4772a27c64d8331
aligurbuz <galiant781@gmail.com>
2017-08-31 19:39:44 +0000

    apix structured

--- lib/spl_autoload_register.php
+++ lib/spl_autoload_register.php
@@ -10,35 +10,68 @@
 |
 */
 
-// Use default autoload implementation
-spl_autoload_register(function($class) {
+class autoloadRegister {
+
+    /**
+     * @var $class
+     */
+    private $class;
+
+    /**
+     * @var $classPath
+     */
+    private $classPath;
 
 
-    $class=root.'/'.$class.'.php';
+    public function register(){
 
-    $class=str_replace("\\","/",$class);
-
-    if(!file_exists($class)){
-
-        $alias=str_replace(root.'/','',$class);
-        $alias=str_replace('.php','',$alias);
-
-        //check system app control for class alias
-        $systemApp=\src\store\config\app::getClassAliasLoader();
-        if(defined('app')){
-            $appAlias='\\src\\app\\'.app.'\\'.version.'\\config\\app';
-            $systemApp=array_merge($systemApp,$appAlias::getAppClassAlias());
-        }
-
-
-
-        //set class_alias groups
-        if(array_key_exists($alias,$systemApp)){
-            class_alias($systemApp[$alias],$alias);
-        }
-    }
-    else{
-        require($class);
+        // Use default autoload implementation
+        spl_autoload_register(function($class){
+            $this->getCallBackVar($class);
+            $this->registerCallBackFormatter();
+        });
     }
 
-});
+    private function getCallBackVar($class){
+
+        $this->class=$class;
+        $this->classPath=root.'/'.$this->class.'.php';
+        $this->classPath=str_replace("\\","/",$this->classPath);
+    }
+
+    private function registerCallBackFormatter () {
+
+        $this->checkAliasClassFormatter($this->classPath,function() {
+            require($this->classPath);
+        });
+    }
+
+
+    private function checkAliasClassFormatter($class,$callback){
+
+        if(!file_exists($class)){
+
+            $alias=str_replace(root.'/','',$class);
+            $alias=str_replace('.php','',$alias);
+
+            //check system app control for class alias
+            $systemApp=\src\store\config\app::getClassAliasLoader();
+            if(defined('app')){
+                $appAlias='\\src\\app\\'.app.'\\'.version.'\\config\\app';
+                $systemApp=array_merge($systemApp,$appAlias::getAppClassAlias());
+            }
+
+            //set class_alias groups
+            if(array_key_exists($alias,$systemApp)){
+                class_alias($systemApp[$alias],$alias);
+            }
+        }
+        else {
+            return call_user_func($callback);
+        }
+    }
+
+}
+
+echo (new autoloadRegister())->register();
+
