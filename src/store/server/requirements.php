2e713caaed62abad1b88a9c4679c03fd4d22e9f3
aligurbuz <galiant781@gmail.com>
2017-09-02 10:25:57 +0000

    apix structured

---
+++ src/store/server/requirements.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Server requirement
+ * With this server command, you can install all the system requirements for the application.
+ * You can view your list on a list. You can load your requirements with all of the yards,
+ * which can run one by one according to the master key.
+ */
+
+return [
+
+    'showList'=>[
+        'ls-l'
+    ]
+];
