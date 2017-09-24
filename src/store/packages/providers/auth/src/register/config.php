d0775853981209325569d8e21634e8cf76eff65d
aligurbuz <galiant781@gmail.com>
2017-09-24 17:02:08 +0000

    apix structured

--- src/store/packages/providers/auth/src/register/config.php
+++ src/store/packages/providers/auth/src/register/config.php
@@ -1,11 +1,4 @@
 <?php
-/*
- * This is the official PHP client for Elasticsearch. It is designed to be a very low-level client that does not stray from the REST API.
- * All methods closely match the REST API, and furthermore, match the method structure of other language clients (ruby, python, etc).
- * We hope that this consistency makes it easy to get started with a client, and to seamlessly switch from one language to the next with minimal effort.
- * The client is designed to be "unopinionated". There are a few universal niceties added to the client (cluster state sniffing,
- * round-robin requests, etc) but largely it is very barebones. This was intentional. We want a common base that more sophisticated libraries can build on top of.
- */
 
 namespace src\store\packages\providers\auth\src\register;
 
@@ -31,9 +24,12 @@ abstract class config {
      */
     public function getAuthHash($config){
 
-        //push to array auth credentials
-        //push to array auth client ip
-        //push to array auth client http browser info
+        /**
+         * @var $config \src\store\packages\providers\auth\src\config
+         * push to array auth credentials
+         * push to array auth client ip
+         * push to array auth client http browser info
+         */
         $this->dataForHash              =$config->getCredentials();
         $this->dataForHash['ip']        =(new Request())->getClientIp();
         $this->dataForHash['agent']     =$_SERVER['HTTP_USER_AGENT'];
